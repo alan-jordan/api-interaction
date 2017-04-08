207f28212bac899d9864921e05012edb0e73db6a
Alan Jordan <alanpjordan@gmail.com>
2017-04-08 15:24:23 +1200

    Added syling. Also added bootstrap to get a template together quickly

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', "https://eda-te-reo.herokuapp.com/api/proverbs", false);
-xhr.send();
-var response = JSON.parse(xhr.responseText);
-console.log(response);
+var button = document.getElementById("button");
+var proverb = document.getElementById("source");
+var translation = document.getElementById("translation");
 
-var source_text = documenet.getElementById("source");
-var translation_text = documenet.getElementById("translation");
+button.addEventListener('click', getNewProverb);
+window.onload = getNewProverb();
+
+function getNewProverb() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "https://eda-te-reo.herokuapp.com/api/proverbs", false);
+  xhr.send();
+  var response = JSON.parse(xhr.responseText);
+  proverb.innerHTML = "<p><strong>Proverb:</strong> " + response.source + "</p>";
+  translation.innerHTML = "<p><strong>Translation:</strong> " + response.translation + "</p>";
+}
